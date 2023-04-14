f87c11351ac944d02a08b168f8b8fc461b6d4966
Chris <104800006+renners7777@users.noreply.github.com>
2023-04-14 22:18:45 +0100

    Changes added

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ app.get("/api/:rapperName", (req, res) => {
     }
 })
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is running on port ${PORT}! You better go catch it!`)
 })
